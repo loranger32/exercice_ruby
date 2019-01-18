95918cde2aca317a8ef6d7758363d289413a0a4b
loranger32 <laurentguinotte@gmail.com>
2019-01-18 18:11:05 +0100

    Largest serie product - second refactoring

--- largest-series-product/largest_series_product.rb
+++ largest-series-product/largest_series_product.rb
@@ -1,44 +1,33 @@
 class Series
   def initialize(string)
-    raise ArgumentError if string.match?(/[a-z]/i)
+    raise ArgumentError if string.match?(/\D/)
     @digits = string.chars.map(&:to_i)
   end
 
-  def largest_product(slice_length)
-    return 1 if slice_length.zero?
-    return 0 if digits_are_all_zeros?
-    
-    validate_arguments(slice_length)
-    
-    compute_max_product(slice_length)
+  def largest_product(span)
+    return 1 if span.zero?
+    validate_arguments(span)
+    compute_max_product(span)
   end
 
   private
 
   attr_reader :digits
 
-  def digits_are_all_zeros?
-    !digits.empty? && digits.all? { |digit| digit == 0 }
+  def validate_arguments(span)
+    raise ArgumentError if argument_bigger_than_string_size?(span) ||
+      empty_string_and_nonzero_span?(span) || span < 0
   end
 
-  def validate_arguments(slice_length)
-    raise ArgumentError if argument_bigger_than_string_size?(slice_length) ||
-      empty_string_and_nonzero_span?(slice_length) || slice_length < 0
+  def argument_bigger_than_string_size?(span)
+    span > digits.size
   end
 
-  def empty_string_and_nonzero_span?(slice_length)
-    digits.empty? && slice_length > 0
+  def empty_string_and_nonzero_span?(span)
+    digits.empty? && span > 0
   end
 
-  def argument_bigger_than_string_size?(slice_length)
-    slice_length > digits.size
-  end
-
-  def compute_max_product(slice_length)
-    result = []
-    digits.each_cons(slice_length) do |serie|
-      result << serie.reduce(:*)
-    end
-    result.max
+  def compute_max_product(span)
+    digits.each_cons(span).map { |serie| serie.reduce(:*) }.max
   end
 end
