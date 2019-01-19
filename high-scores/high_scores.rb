c89d557b859022cf49ec9c430a283231e7ccf0a4
Laurent G <laurentguinotte@gmail.com>
2019-01-19 19:01:07 +0100

    High Scores - Refactor according to mentor suggestion

--- high-scores/high_scores.rb
+++ high-scores/high_scores.rb
@@ -14,19 +14,16 @@ class HighScores
   end
 
   def personal_top
-    if scores.size >= 3
-      scores.sort[-3..-1].reverse
-    else
-      scores.sort.reverse
-    end
+    scores.max(3)
   end
 
   def report
+    output = "Your latest score was #{latest}. "
     if latest == personal_best
-      "Your latest score was #{personal_best}. That's your personal best!"
+      output + "That's your personal best!"
     else
       difference = personal_best - latest
-      "Your latest score was #{latest}. That's #{difference} short of your personal best!"
+      output + "That's #{difference} short of your personal best!"
     end
   end
 end
