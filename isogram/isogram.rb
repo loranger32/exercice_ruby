ca9c345e43da30b36e428785cf2dee5593bdda9d
Laurent G <laurentguinotte@gmail.com>
2019-01-31 19:10:17 +0100

    Isogram Refactor code

--- isogram/isogram.rb
+++ isogram/isogram.rb
@@ -1,6 +1,16 @@
 module Isogram
-  def self.isogram?(input)
-    characters = input.gsub(/-|\s/, '').downcase.chars
-    characters.size == characters.uniq.size
+  module_function
+
+  def isogram?(input)
+    letters = format_input(input)
+    unique_letters?(letters)
+  end
+
+  def format_input(input)
+    input.gsub(/-|\s/, '').downcase.chars
+  end
+
+  def unique_letters?(letters)
+    letters.size == letters.uniq.size
   end
 end
