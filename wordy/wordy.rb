73e46c41ed58558da0cefc2a8717f6d902184028
Laurent G <laurentguinotte@gmail.com>
2019-02-07 17:56:22 +0100

    Wordy - Functionnal - Long form

--- wordy/wordy.rb
+++ wordy/wordy.rb
@@ -1,37 +1,99 @@
-require 'pry'
+class TokenError < StandardError; end
 
-class WordProblem
+class ProblemDescriptionValidator
+
   FIRST_WORDS = "What is ".freeze
-  TOKENS = /\d+|\s+|plus|\?|-/i
-
-  attr_reader :problem_in_words
+  LAST_TOKEN = '?'.freeze
+  VALID_TOKENS = /\d+|\s+|plus|minus|multiplied|divided|by|-/i
 
   def initialize(problem_in_words)
-    @problem_in_words = problem_in_words
-    valid_input?(problem_in_words)
+    check_begin_and_end_of(problem_in_words)
+    @input = extract_core_data_from(problem_in_words)
   end
 
-  def answer
-
+  def validate_and_extract_data
+    validate_input
+    @input
   end
 
   private
 
-  def valid_input?(problem_in_words)
-    raise ArgumentError unless begins_with_correct_words? && valid_tokens?
+  def check_begin_and_end_of(problem_in_words)
+    error_msg = "Problem description must start with 'What is ' and end with '?'"
+    raise ArgumentError,  error_msg unless \
+      problem_in_words.start_with?(FIRST_WORDS) &&
+        problem_in_words.end_with?(LAST_TOKEN)
   end
 
-  def begins_with_correct_words?
-    problem_in_words.start_with?(FIRST_WORDS)
+  def extract_core_data_from(problem)
+    problem.delete_prefix(FIRST_WORDS).chop
   end
 
-  def valid_tokens?
-    problem_in_words.delete(FIRST_WORDS).split.each do |char|
-      binding.pry
-      return false unless char.match?(TOKENS)
+  def validate_input
+    @input.split.each do |token|
+      raise TokenError, "Token: '#{token}' is invalid" unless token.match?(VALID_TOKENS)
     end
-    true
   end
 end
 
-p WordProblem.new("What is 1 plus 1 and?")
+class Tokenizer
+  def initialize(input)
+    @input = input.split.delete_if { |token| token == 'by' }
+  end
+
+  def tokenize
+    converted_tokens = []
+    @input.each do |token|
+      if token.match?(/\d+/)
+        converted_tokens << token.to_i
+      else
+        converted_tokens << convert_operation_symbol(token)
+      end
+    end
+    converted_tokens
+  end
+
+  private
+
+  def convert_operation_symbol(token)
+    case token
+    when 'plus'       then :+
+    when 'minus'      then :-
+    when 'multiplied' then :*
+    when 'divided'    then :/
+    else
+      raise TokenError, "Unrecognized token: #{token}."
+    end
+  end
+end
+
+class Processor
+  def initialize(tokens)
+    @tokens = tokens
+  end
+
+  def process
+    # Initialize the result with te first number
+    result = @tokens.shift
+
+    # In each slice of two elements, the first is the operation the second the operand 
+    @tokens.each_slice(2) do |slice|
+      result = result.send(slice[0], slice[1])
+    end
+    result
+  end
+end
+
+class WordProblem
+  def initialize(problem_in_words)
+    validator = ProblemDescriptionValidator.new(problem_in_words)
+    @input = validator.validate_and_extract_data
+
+    @tokens = Tokenizer.new(@input).tokenize
+  end
+
+  def answer
+    processor = Processor.new(@tokens)
+    processor.process
+  end
+end
