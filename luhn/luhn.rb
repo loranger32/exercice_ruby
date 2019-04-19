527f3aa2963034037497145a3baa76f540876916
Laurent G <laurentguinotte@gmail.com>
2019-04-19 11:02:34 +0200

    Improve Luhn according to mentor feed-back

--- luhn/luhn.rb
+++ luhn/luhn.rb
@@ -1,47 +1,46 @@
-class Luhn
-  attr_reader :stripped_string, :has_invalid_input, :checksum
+module Luhn
+  module_function
 
-  def initialize(string_number)
-    @stripped_string = string_number.gsub(/\s/, '')
-    @has_invalid_input = invalid_syntax?
-    @checksum = calculate_checksum  
+  def valid?(string_number)
+    formatted_string = format_string(string_number)
+    return false if invalid_string?(formatted_string)
+    
+    checksum = calculate_checksum_from(formatted_string)
+    valid_checksum?(checksum)
   end
 
-  def self.valid?(string_number)
-    validator = new(string_number)
-    return false if validator.has_invalid_input
-
-    validator.valid_checksum?
+  def format_string(string_number)
+    string_number.gsub(/\s/, '')
   end
 
-  def invalid_syntax?
-    one_digit_only? || has_a_non_digit_character?
+  def invalid_string?(formatted_string)
+    one_digit_only?(formatted_string) || has_a_non_digit_character?(formatted_string)
   end
 
-  def one_digit_only?
-    stripped_string.size <= 1
+  def one_digit_only?(formatted_string)
+    formatted_string.size <= 1
   end
 
-  def has_a_non_digit_character?
-    stripped_string.match?(/\D/)
+  def has_a_non_digit_character?(formatted_string)
+    formatted_string.match?(/\D/)
   end
 
-  def calculate_checksum
+  def calculate_checksum_from(formatted_string)
     # 1. Convert string to reversed array of integers
     # 2. Doubble every other digit from the right
     # 3. substract 9 from digit if it is greater than 9
     # 4. return the sum of all digits
-    stripped_string.to_i.digits.map.with_index do |digit, index|
+    formatted_string.to_i.digits.map.with_index do |digit, index|
       if index.odd?
         digit *= 2
         digit > 9 ? digit - 9 : digit
       else
         digit
       end
-    end.reduce(:+)
+    end.sum
   end
 
-  def valid_checksum?
+  def valid_checksum?(checksum)
     checksum % 10 == 0
   end
 end
